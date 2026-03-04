d2604e8430e55fa21dcfd9281774190687d65e0c
Abd2lhakam19 <midoahme93@gmail.com>
2026-03-04 02:39:15 +0200

    feat: update BestSellerItem to accept props for dynamic content

--- src/components/BestSellerItem.jsx
+++ src/components/BestSellerItem.jsx
@@ -1,10 +1,10 @@
 import Icon from "./Icon";
-function BestSellerItem({}) {
+function BestSellerItem({ img, title, price }) {
     return (
         <div>
             <div className=" group relative  h-100 rounded-2xl overflow-hidden mb-3   ">
                 <div className=" absolute inset-0 bg-center bg-cover group-hover:scale-105 transition-transform duration-700 " style={{
-                    backgroundImage: "url('/cloths.png')",
+                    backgroundImage: `url(${img})`,
                     display: 'block'
 
                 }}>
@@ -15,8 +15,8 @@ function BestSellerItem({}) {
                 <button className="absolute bottom-4 left-4 right-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer bg-blue-700 px-10 py-3 rounded-full text-white capitalize font-bold text-sm"> add to cart</button>
             </div>
             <div className="flex flex-col text-start gap-0.5">
-                <h3 className="font-bold text-slate-900 capitalize">Midnight Wool Blazer</h3>
-                <p className="text-sm text-slate-500">$495.00</p>
+                <h3 className="font-bold text-slate-900 capitalize">{title}</h3>
+                <p className="text-sm text-slate-500">{price}</p>
             </div>
         </div>
     );
